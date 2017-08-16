361eb6c531233de2f87015fc61e8e63f1adaacf7
Brandon Davis <brandon_davis5@comcast.com>
2017-08-16 08:48:04 -0600

    add solution for sendMessageToClient

--- src/main/kotlin/KotlinMethods.kt
+++ src/main/kotlin/KotlinMethods.kt
@@ -14,7 +14,12 @@ interface Mailer {
 
 // Rewrite Java method 'SampleJavaMethods.sendMessageToClient' in Kotlin
 fun sendMessageToClient(client: Client?, message: String?, mailer: Mailer) {
-    TODO()
+    if (client == null || message == null) return
+
+    val personalInfo = client.personalInfo ?: return
+    val email = personalInfo.email ?: return
+
+    mailer.sendMessage(email, message)
 }
 
 
